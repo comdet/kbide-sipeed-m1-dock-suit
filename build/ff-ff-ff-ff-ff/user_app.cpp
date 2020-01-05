b63d27958521cbe4a0c19c8a60fa790bd3ff2b57
Max <max30012540@hotmail.com>
2020-01-05 09:30:00 +0700

    Initial commit

---
+++ build/ff-ff-ff-ff-ff/user_app.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+typedef int Number;
+typedef int Boolean;
+
+
+
+
+
+
+
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+  
+}
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "sipeed-m1-dock-suit",
+  platform: "maixduino",
+  title: "Sipeed M1 dock suit",
+  description: "AI at the edgeAI is pervasive today, from consumer to enterprise applications",
+  author: "Sonthaya Nongnuch",
+  website: "https://www.ioxhop.com/",
+  email: "max30012540@hotmail.com",
+  git: "https://github.com/ioxhop/kbide-sipeed-m1-dock-suit",
+  image: "/static/display.png",
+  version: "1.0.0"
+};
